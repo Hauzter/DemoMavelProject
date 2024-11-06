4549521274e408674f0ce41f2969196c850c81a8
Hauzter <kemdanuoclanh@gmail.com>
2024-11-06 23:42:27 +0700

    init project

---
+++ mavel-project/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+
+// const data = [
+//   { name: "Anom", age: 19, gender: "Male" },
+//   { name: "Megha", age: 19, gender: "Female" },
+//   { name: "Subham", age: 25, gender: "Male" },
+// ]
+export const App = () => {
+  const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://gateway.marvel.com/v1/public/characters?ts=1667722238&apikey=df17d2ee72f64e3bea59f98b87e57bb9&hash=18b5a5757dc9077ff8f8fbab26a6a4fb')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data: ', error));
+    }, []);
+  return (
+    <div className="App">
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Modified</th>
+                </tr>
+                {data.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.id}</td>
+                            <td>{val.name}</td>
+                            <td>{val.modified}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        </div>
+  );
+}
+
+export default App;
