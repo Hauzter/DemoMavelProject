c67fc239906eb2a53566b6f07200f8d70b2672c8
Hauzter <kemdanuoclanh@gmail.com>
2024-11-14 01:02:40 +0700

    update backend

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,8 @@
-fetch("http://gateway.marvel.com/v1/public/characters?ts=1667722238&apikey=df17d2ee72f64e3bea59f98b87e57bb9&hash=18b5a5757dc9077ff8f8fbab26a6a4fb")
+// List Chatacters
+fetch('http://localhost:3000/characterList')
     .then(response => response.json())
     .then(data => {
-        const characters = data.data.results;
+        const characters = data;
         const tableBody = document.querySelector("#characters-table tbody");
 
         characters.forEach(character => {
@@ -10,15 +11,36 @@ fetch("http://gateway.marvel.com/v1/public/characters?ts=1667722238&apikey=df17d
             row.insertCell().textContent = character.name;
             row.insertCell().textContent = character.modified;
             const detailCell = row.insertCell();
-                const detailButton = document.createElement("button");
-                detailButton.textContent = "View Details";
-                detailButton.addEventListener("click", () => {
-                    window.location.href = `character.html?id=${character.id}`;
-                });
-                detailCell.appendChild(detailButton);
+            const detailButton = document.createElement("button");
+            detailButton.textContent = "View Details";
+            detailButton.addEventListener("click", () => {
+                fetch('http://localhost:3000/sendCharacterId', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ characterId: character.id })
+                })
+                    .then(response => {
+                        // localStorage.setItem('charactersData', JSON.stringify(charactersData));
+                        if (response.ok) {
+                            console.log('Character ID sent to the server successfully.');
+                            // Sau khi xử lý thành công, chuyển hướng đến trang character.html
+                            window.location.href = `character.html`;
+                        } else {
+                            throw new Error('Failed to send Character ID to the server.');
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                    });
+                // window.location.href = `character.html?id=${character.id}`;
+            });
+
+            detailCell.appendChild(detailButton);
         });
     }
         //console.log(data.data)
 
     )
-    .catch (error => console.error(error));
+    .catch(error => console.error(error));
