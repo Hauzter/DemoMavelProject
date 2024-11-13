6247b69179979240a1c352c7f6131874e84503c8
Hauzter <kemdanuoclanh@gmail.com>
2024-11-14 01:20:49 +0700

    update code

--- BackendCheck/express-api/index.js
+++ BackendCheck/express-api/index.js
@@ -8,35 +8,33 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/characterList', async (req, res) => {
-    try {
-        const response = await fetch('http://gateway.marvel.com/v1/public/characters?ts=1667722238&apikey=df17d2ee72f64e3bea59f98b87e57bb9&hash=18b5a5757dc9077ff8f8fbab26a6a4fb');
-        const data = await response.json();
+app.get('/characterList', (req, res) => {
+        fetch('http://gateway.marvel.com/v1/public/characters?ts=1667722238&apikey=df17d2ee72f64e3bea59f98b87e57bb9&hash=18b5a5757dc9077ff8f8fbab26a6a4fb')
+        .then(response => response.json())
+        .then(data => {
+            const charactersData = data.data.results.map(character => ({
+                id: character.id,
+                name: character.name,
+                modified: character.modified,
+                thumbnail: {
+                    path: character.thumbnail.path,
+                    extension: character.thumbnail.extension
+                }
+            }));
 
-        const charactersData = data.data.results.map(character => ({
-            id: character.id,
-            name: character.name,
-            modified: character.modified,
-            thumbnail: {
-                path: character.thumbnail.path,
-                extension: character.thumbnail.extension
-            }
-        }));
-
-        res.json(charactersData);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
+            res.json(charactersData);
+        })
+        .catch(error => console.error(error));
 });
 let characterId;
 app.post('/sendCharacterId', (req, res) => {
     characterId = req.body.characterId;
     console.log('Received characterId:', characterId);
     // Xử lý giá trị characterId ở đây
-    
-    // Trả về phản hồi cho máy khách (client)
+
+    // Trả về phản hồi cho client
     res.send('Character ID received successfully');
-    
+
 });
 
 
@@ -63,7 +61,7 @@ app.post('/sendCharacterId', (req, res) => {
 // });
 
 
-app.get('/characterDetail', async (req, res) => {
+app.get('/characterDetail', (req, res) => {
     fetch(`http://gateway.marvel.com/v1/public/characters/${characterId}?ts=1667722238&apikey=df17d2ee72f64e3bea59f98b87e57bb9&hash=18b5a5757dc9077ff8f8fbab26a6a4fb`)
         .then(response => response.json())
         .then(data => {
@@ -76,7 +74,7 @@ app.get('/characterDetail', async (req, res) => {
                     extension: character.thumbnail.extension
                 }
             }));
-    
+
             res.json(charactersData);
         })
         .catch(error => console.error(error));
